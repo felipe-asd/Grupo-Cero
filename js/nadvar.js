3f60039bf72da8f50e116cbed5df6dc2dde7cca1
Ale <sistemarestobar@hotmail.com>
2021-06-03 01:26:34 -0400

    actualizando autores

--- js/nadvar.js
+++ js/nadvar.js
@@ -14,49 +14,49 @@ $(document).ready(function() {
         Categorias
       </a>
       <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-        <li><a class="dropdown-item" href="../categorias/categoria_escultores.html">Categoria Escultores</a></li>
-        <li><a class="dropdown-item" href="../categorias/categoria_orfebres.html">Categoria Orfebres</a></li>
-        <li><a class="dropdown-item" href="../categorias/categoria_pintores.html">Categoria Pintores</a></li>
-        <li><a class="dropdown-item" href="../categorias/categoria_tejedor.html">Categoria Tejedores</a></li>
+        <li><a class="dropdown-item" href="../../categorias/categoria_escultores.html">Categoria Escultores</a></li>
+        <li><a class="dropdown-item" href="../../categorias/categoria_orfebres.html">Categoria Orfebres</a></li>
+        <li><a class="dropdown-item" href="../../categorias/categoria_pintores.html">Categoria Pintores</a></li>
+        <li><a class="dropdown-item" href="../../categorias/categoria_tejedor.html">Categoria Tejedores</a></li>
       </ul>
     <li class="nav-item dropdown">
       <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
         Galerias
       </a>
       <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-        <li><a class="dropdown-item" href="../galerias/galeria_generica_Van_Gogh.html">Galeria de Vincent van Gogh</a></li>
-        <li><a class="dropdown-item" href="../galerias/galeria_generica_Picasso.html">Galeria de Picasso</a></li>
-        <li><a class="dropdown-item" href="../galerias/galeria_generica_Andres_Lupin.html">Galeria de Andres</a></li>
-        <li><a class="dropdown-item" href="../galerias/galeria_generica_Donatello.html">Galeria de Donatello</a></li>
-        <li><a class="dropdown-item" href="../galerias/galeria_generica_Leandro.html">Galeria de Leandro</a></li>
-        <li><a class="dropdown-item" href="../galerias/galeria_generica_Marcelo.html">Galeria de Marcelo</a></li>
-        <li><a class="dropdown-item" href="../galerias/galeria_generica_Miguel_angel.html">Galeria de Miguel Angel</a></li>
-        <li><a class="dropdown-item" href="../galerias/galeria_generica_Pablo.html">Galeria de Pablo</a></li>
+        <li><a class="dropdown-item" href="../../galerias/galeria_generica_Van_Gogh.html">Galeria de Vincent van Gogh</a></li>
+        <li><a class="dropdown-item" href="../../galerias/galeria_generica_Picasso.html">Galeria de Picasso</a></li>
+        <li><a class="dropdown-item" href="../../galerias/galeria_generica_Andres_Lupin.html">Galeria de Andres</a></li>
+        <li><a class="dropdown-item" href="../../galerias/galeria_generica_Donatello.html">Galeria de Donatello</a></li>
+        <li><a class="dropdown-item" href="../../galerias/galeria_generica_Leandro.html">Galeria de Leandro</a></li>
+        <li><a class="dropdown-item" href="../../galerias/galeria_generica_Marcelo.html">Galeria de Marcelo</a></li>
+        <li><a class="dropdown-item" href="../../galerias/galeria_generica_Miguel_angel.html">Galeria de Miguel Angel</a></li>
+        <li><a class="dropdown-item" href="../../galerias/galeria_generica_Pablo.html">Galeria de Pablo</a></li>
       </ul>
     <li class="nav-item dropdown">
       <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
         Artistas
       </a>
       <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-        <li><a class="dropdown-item" href="../Pintores/ficha_autor_Van_Gogh.html">Vincent van Gogh</a></li>
-        <li><a class="dropdown-item" href="../Pintores/ficha_autor_Picasso.html">Picasso</a></li>
-        <li><a class="dropdown-item" href="../escultores/donatello.html">Donatello</a></li>
-        <li><a class="dropdown-item" href="../escultores/miguel_angel.html">Miguel Angel</a></li>
-        <li><a class="dropdown-item" href="../orfebres/marcelo_cumplido.html">Marcelo</a></li>
-        <li><a class="dropdown-item" href="../orfebres/pablo_rodriguez.html">Pablo</a></li>
-        <li><a class="dropdown-item" href="../tejedores/leandro_bonatelli.html">Leandro</a></li>
-        <li><a class="dropdown-item" href="../tejedores/andres_lupin.html">Andres Lupin</a></li>
+        <li><a class="dropdown-item" href="../../Pintores/ficha_autor_Van_Gogh.html">Vincent van Gogh</a></li>
+        <li><a class="dropdown-item" href="../../Pintores/ficha_autor_Picasso.html">Picasso</a></li>
+        <li><a class="dropdown-item" href="../../escultores/donatello.html">Donatello</a></li>
+        <li><a class="dropdown-item" href="../../escultores/miguel_angel.html">Miguel Angel</a></li>
+        <li><a class="dropdown-item" href="../../orfebres/marcelo_cumplido.html">Marcelo</a></li>
+        <li><a class="dropdown-item" href="../../orfebres/pablo_rodriguez.html">Pablo</a></li>
+        <li><a class="dropdown-item" href="../../tejedores/leandro_bonatelli.html">Leandro</a></li>
+        <li><a class="dropdown-item" href="../../tejedores/andres_lupin.html">Andres Lupin</a></li>
       </ul>
       <li class="nav-item">
-          <a class="nav-link" href="../vistas/Iniciar_sesion.html">Iniciar sesion</a>
+          <a class="nav-link" href="../../vistas/Iniciar_sesion.html">Iniciar sesion</a>
         </li>
         
        <li class="nav-item">
-          <a class="nav-link" href="../vistas/Registrarse.html">Resgistrarse</a>
+          <a class="nav-link" href="../../vistas/Registrarse.html">Resgistrarse</a>
         </li>
         
         <li class="nav-item">
-          <a class="nav-link" href="../vistas/Formulario.html">Contacto</a>
+          <a class="nav-link" href="../../vistas/Formulario.html">Contacto</a>
   
         </li>
   </ul>
