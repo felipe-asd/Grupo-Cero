f5613c3871830b0db18838fee1530f79772fde2f
Ale <sistemarestobar@hotmail.com>
2021-06-03 20:32:31 -0400

    indetacion codigo y comentarios

--- js/geolocalizacion.js
+++ js/geolocalizacion.js
@@ -1,33 +1,29 @@
-$(document).ready(function(){
-    function showPosition(position){
-      var lat = position.coords.latitude;
-      var lon = position.coords.longitude;
+$(document).ready(function () {
+  function showPosition(position) {
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
 
-     console.log(lat)
-     console.log(lon)
-     
-     $.get(
-    `https://www.metaweather.com/api/location/search/?lattlong=${lat},${lon}`,
-    function (data) {
-      $.get(
-        `https://www.metaweather.com/api/location/${data[0].woeid}`,
-        function (weather) {
-          $('#futer').html(`TEMPERATURA: <img  src='https://www.metaweather.com/static/img/weather/${weather.consolidated_weather[0].weather_state_abbr}.svg' height='25px'>
+    console.log(lat)
+    console.log(lon)
+
+    $.get(`https://www.metaweather.com/api/location/search/?lattlong=${lat},${lon}`,function (data){
+      console.log('https://www.metaweather.com/api/location/search/?lattlong=' + lat + ',' + lon);
+
+      $.get(`https://www.metaweather.com/api/location/${data[0].woeid}`,function (weather) {
+        console.log("https://www.metaweather.com/api/location/" + data[0].woeid); 
+        $('#futer').html(`TEMPERATURA: <img  src='https://www.metaweather.com/static/img/weather/${weather.consolidated_weather[0].weather_state_abbr}.svg' height='25px'>
           ${weather.consolidated_weather[0].the_temp}°C`);
-        }
-      );
-    }
-  );
-    }
-  if (navigator.geolocation){
-          
-          navigator.geolocation.getCurrentPosition(function(posision){
-            showPosition(posision);
-          })
-        
-        }else{
-          alert("navergador no soporta la geolocalizacion")
-        }
-  
-  })
-  
+      });
+    });
+  }
+  if (navigator.geolocation) {
+
+    navigator.geolocation.getCurrentPosition(function (posision) {
+      showPosition(posision);
+    })
+
+  } else {
+    alert("navergador no soporta la geolocalizacion")
+  }
+
+})
